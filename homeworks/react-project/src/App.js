8137fc9da7623d806cdc6babf84a3e5e60d61d41
vladyakubets <jakubetc.vlad@gmail.com>
2021-05-11 19:34:04 +0300

    react classwork 4

--- homeworks/react-project/src/App.js
+++ homeworks/react-project/src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Cast from "./components/Cast/Cast";
+import CastPersonDetails from "./components/CastPerson/CastPersonDetails";
 
 function App() {
-  return (
-    <div>
+    return (
+        <div>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/cast">Cast</Link>
+                            </li>
+                            <li>
+                                <Link to="/inventory">Inventory</Link>
+                            </li>
 
-    </div>
-  );
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route exact={true} path="/cast" render={
+                            ({match:{url}})=>{
+                                return <Cast url={url}/>
+                            }
+                        }/>
+                        <Route exact={true} path={"/cast/aboutPerson:id"} render={({match:{params:{id}}})=>{
+                            return <CastPersonDetails personId={id}/>
+                        }}/>
+
+                        <Route exact={true} path="/inventory"/>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
