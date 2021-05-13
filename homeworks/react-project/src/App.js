1037156c68eb11f86fcd030377a44e451a9d5230
vladyakubets <jakubetc.vlad@gmail.com>
2021-05-13 20:56:06 +0300

    react classwork 5

--- homeworks/react-project/src/App.js
+++ homeworks/react-project/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Users from "./users/Users";
+import UserDetails from "./users/user/userDetails/UserDetails";
 
 function App() {
   return (
-    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
 
-    </div>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/" render={(props)=> {
+              return <Users />
+            }}/>
+              <Route path="/user/:id" render={(props)=> {
+              return <UserDetails {...props} />
+            }}/>
+
+
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
