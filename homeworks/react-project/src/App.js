3d50a868e909e4b96fc4470f0d1cb1861cc45035
vladyakubets <jakubetc.vlad@gmail.com>
2021-05-18 19:09:12 +0300

    classwork 6 react

--- homeworks/react-project/src/App.js
+++ homeworks/react-project/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 
-function App() {
-  return (
-    <div>
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Counter from "./components/Counter/Counter";
+import Home from "./components/Home/Home";
 
-    </div>
-  );
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/counter">Counter</Link>
+                        </li>
+
+                    </ul>
+                </nav>
+
+
+                <Switch>
+                    <Route path="/counter" component={Counter}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
